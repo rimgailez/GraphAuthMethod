22c8930e2ad26edc49ac3f1c7e5010555cd2c8d0
rimgailez <rznutaite@gmail.com>
2022-05-04 22:11:22 +0300

    Made buttons round and changed one color

--- app/src/main/java/vu/university/graphauthmethod/constants/Colors.java
+++ app/src/main/java/vu/university/graphauthmethod/constants/Colors.java
@@ -12,7 +12,7 @@ public final class Colors {
 
     public final int[] additionalColors = new int[] {Color.rgb(255,117,163), Color.rgb(215, 175, 250),
             Color.rgb(0, 153, 153), Color.rgb(190, 232, 106),
-            Color.rgb(128,128,128), Color.rgb(255, 133, 108),
+            Color.rgb(128,128,128), Color.rgb(242, 187, 166),
             Color.rgb(153,0,76), Color.rgb(155, 53, 2)};
 
     public int[] getAllColors(){
